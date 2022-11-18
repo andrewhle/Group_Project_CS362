2c3dc56c49998fda8b6c35fc8dc3627568d18650
Jon Ramm <jonwramm@gmail.com>
2022-11-18 16:47:16 -0700

    add date conversion functionality and tests

--- task.py
+++ task.py
@@ -11,7 +11,59 @@ def my_datetime(num_sec):
     since the epoch, January 1st 1970, and returns that date as a
     string in the format MM-DD-YYY.
     """
-    return "01-01-1970"
+    days_in_months = {
+        "01": 31,
+        "02": 28,
+        "03": 31,
+        "04": 30,
+        "05": 31,
+        "06": 30,
+        "07": 31,
+        "08": 31,
+        "09": 30,
+        "10": 31,
+        "11": 30,
+        "12": 31
+    }
+    seconds_in_day = 86400
+    days = num_sec // seconds_in_day
+    year, day_count = calc_year_and_days(days)
+    day = 0
+    for month in days_in_months:
+        if day_count == 0:
+            return month + "-" + str(day) + "-" + str(year)
+        while day < days_in_months[month]:
+            if day_count == 0:
+                day_str = str(day)
+                if len(day_str) == 1:
+                    day_str = "0" + day_str
+                return month + "-" + day_str + "-" + str(year)
+            day += 1
+            day_count -= 1
+        day = 0
+
+
+def calc_year_and_days(days):
+    """Takes a number of days and returns the year since the epoch
+    and the amount of days until the exact date.
+    """
+    year = 1970
+    day_count = 1
+    for _ in range(days):
+        day_count += 1
+        if day_count == 366:
+            year += 1
+            day_count = 0
+        if day_count == 365:
+            if year % 4 == 0:
+                if year % 100 == 0:
+                    if year % 400 != 0:
+                        year += 1
+                        day_count = 0
+            else:
+                year += 1
+                day_count = 0
+    return (year, day_count)
 
 
 def conv_endian(num, endian='big'):
--- tests.py
+++ tests.py
@@ -7,13 +7,47 @@ class TestCase(unittest.TestCase):
     def test1(self):
         self.assertTrue(True)
 
+##################################################
+#
+# Tests for conv_num
+#
+##################################################
+
     def test_conv_num_valid(self):
         """Tests for valid return value of function conv_num."""
         self.assertEqual(conv_num('0'), 0)
 
-    def test_my_datetime(self):
+##################################################
+#
+# Tests for my_datetime
+#
+##################################################
+
+    def test_my_datetime_1(self):
         """Tests for valid return of function my_datetime."""
         self.assertEqual(my_datetime(0), '01-01-1970')
+
+    def test_my_datetime_2(self):
+        """Tests that 1234567891 returns '02-13-2009'."""
+        self.assertEqual(my_datetime(1234567891), '02-13-2009')
+
+    def test_my_datetime_3(self):
+        """Tests that 999999 returns '01-12-1970'."""
+        self.assertEqual(my_datetime(999999), '01-12-1970')
+
+    def test_my_datetime_4(self):
+        """Tests that 99999999 returns '03-03-1973'."""
+        self.assertEqual(my_datetime(99999999), '03-03-1973')
+
+    def test_my_datetime_5(self):
+        """Tests that 999999999 returns '09-09-2001'."""
+        self.assertEqual(my_datetime(999999999), '09-09-2001')
+
+##################################################
+#
+# Tests for conv_endian
+#
+##################################################
 
     def test_conv_endian(self):
         """Tests for valid return of function conv_endian."""
