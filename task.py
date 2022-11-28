2bdc89e527e11fc43bfd32dcf80cafbdcd456e3a
Andrew Le <59432144+andrewhle@users.noreply.github.com>
2022-11-28 14:06:51 -0800

    Fix linting

--- task.py
+++ task.py
@@ -124,26 +124,6 @@ def int_conv(num_str):
     return res * negative
 
 
-def int_conv(num_str):
-    """Helper function for conv_num that converts an integer string
-    to an integer.
-    """
-    nums = "1234567890"
-    negative = 1
-    res = 0
-    if len(num_str) == 1 and num_str[0] not in nums:
-        return None
-    for i in range(len(num_str)):
-        if i == 0 and num_str[i] == '-':
-            negative = -1
-            continue
-        if num_str[i] in nums:
-            res = res * 10 + (ord(num_str[i]) - 48)
-        else:
-            return None
-    return res * negative
-
-
 def my_datetime(num_sec):
     """Takes an integer value that represents the number of seconds
     since the epoch, January 1st 1970, and returns that date as a
